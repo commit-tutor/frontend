ca5fe54c0bfbc4e7e144d5977568c63b729bf3a7
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-20 10:58:44 +0900

    chore: absolute path

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import * as path from "node:path";
+import tsconfigPaths from "vite-tsconfig-paths/dist";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
