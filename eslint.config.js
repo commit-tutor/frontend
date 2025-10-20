5ea018eb341327a0bc3913b929675acfdb852dd2
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-20 11:12:27 +0900

    chore: eslint & prettierrc

--- eslint.config.js
+++ eslint.config.js
@@ -3,18 +3,35 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierPlugin from 'eslint-plugin-prettier'
+import { globalIgnores } from 'eslint/config'
 
-export default defineConfig([
+export default tseslint.config([
   globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
-      reactHooks.configs['recommended-latest'],
-      reactRefresh.configs.vite,
     ],
+    plugins: {
+      prettier: prettierPlugin,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
+    },
+    rules: {
+      ...reactHooks.configs['recommended-latest'].rules,
+      ...reactRefresh.configs.vite.rules,
+
+      semi: 'off',
+      quotes: 'off',
+      'comma-dangle': 'off',
+      'max-len': 'off',
+      indent: 'off',
+      'object-curly-spacing': 'off',
+      'array-bracket-spacing': 'off',
+      'eol-last': ['error', 'always'],
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
