0bb3cb5d65ee05acb4419448d4c1be4870c362c7
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-20 11:06:03 +0900

    chore: shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 import * as path from "node:path";
 import tsconfigPaths from "vite-tsconfig-paths/dist";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   resolve: {
