8115bee85fd31c4276f518155de3a99a9d250b6a
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-28 21:31:18 +0900

    style: hover style

--- src/components/common/HeaderComponent.tsx
+++ src/components/common/HeaderComponent.tsx
@@ -2,7 +2,7 @@ export default function HeaderComponent() {
   return (
     <div className="flex w-full items-center py-8 px-20 justify-between bg-white/5">
       <h1 className="text-xl gradient-text font-bold">Commit Tutor</h1>
-      <button className="bg-white/5 border border-border/20 w-max-xl px-8 py-2 rounded-lg">로그인</button>
+      <button className="bg-white/5 border border-border/20 w-max-xl px-8 py-2 rounded-lg cursor-pointer hover:bg-white/10">로그인</button>
     </div>
   )
 }
