71c9abe2679edeaeb59a949f5ea6e999ec2d087c
Jang Ju Hyeong <wkdwngud963@gmail.com>
2025-10-28 19:37:40 +0900

    style: background color

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -2,12 +2,8 @@ import {createRootRoute, Outlet} from '@tanstack/react-router'
 
 export function RootLayout() {
   return (
-    <div className="flex justify-center items-center min-h-screen bg-neutral-800 font-sans">
-      <div className="flex flex-col items-center w-[390px] h-[844px] bg-[#121212] text-white shadow-2xl rounded-3xl overflow-hidden">
-        <div className="flex-1 w-full px-6 pt-6 py-6 overflow-y-auto scrollbar-hide">
-          <Outlet />
-        </div>
-      </div>
+    <div className="w-full h-full bg-slate-950">
+      <Outlet/>
     </div>
   )
 }
